4c304f6608f1bed5465247cf3c728d54907fc0fc
domnic-18 <96937667+domnic-18@users.noreply.github.com>
2024-01-19 14:56:38 +0530

    Update script.js
    new tab

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-
-function getResume(){
-    window.location.href="https://drive.google.com/file/d/16KTPh8rbOTu7KMIR-_XDLg48JaUI_Pk1/view?usp=sharing";
+function getResume() {
+    window.open("https://drive.google.com/file/d/16KTPh8rbOTu7KMIR-_XDLg48JaUI_Pk1/view?usp=sharing", "_blank");
 }
 
-function getCode1(){
-    window.location.href="https://github.com/domnic-18/RFID-based-Door-Access.git";
+function getCode1() {
+    window.open("https://github.com/domnic-18/RFID-based-Door-Access.git", "_blank");
 }
 
-function getCode2(){
-    window.location.href="https://github.com/domnic-18/A2D-MEAN-STACK.git";
+function getCode2() {
+    window.open("https://github.com/domnic-18/A2D-MEAN-STACK.git", "_blank");
 }
 
-function getCode3(){
-    window.location.href="https://github.com/domnic-18/Java-Mini-Project.git";
+function getCode3() {
+    window.open("https://github.com/domnic-18/Java-Mini-Project.git", "_blank");
 }
